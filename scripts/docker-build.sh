4c7c022942af20ffefdd611619a82a32b1f39664
Victor Ros <victor.ros.pro@gmail.com>
2024-07-13 03:35:36 +0200

    Updates build and documentation (#1)
    - Add `version.json` file to add metadata
    - Move `docker-compose` environment variables to `.env.local` file
    - Update build script to publish version and latest tags
    - Update documentation

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -12,7 +12,29 @@ SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 REPOSITORY_DIR_PATH="$(cd -- "${SCRIPT_DIR}/.." || return > /dev/null 2>&1 && pwd -P)"
 
 # Variables
-NGINX_VERSION="${NGINX_VERSION:-"1.25.4-alpine"}"
+NGINX_VERSION="$(jq -r '.config.nginxVersion' version.json)"
+DOCKER_IMAGE_NAME="$(jq -r '.name' version.json)"
+DOCKER_IMAGE_TAG="$(jq -r '.version' version.json)"
+
+echo -e "Build Docker image ${DOCKER_IMAGE_NAME}..."
+
+# Check if multi-arch buildx instance exists
+isBuildxInstanceMultiArchExist=$(docker buildx ls | { grep -q "multi-arch" && echo "true" || echo "false"; })
+
+# Create multi-arch buildx instance if it does not exist
+if [ "${isBuildxInstanceMultiArchExist}" = "false" ]; then
+  echo -e "Creating Docker buildx instance multi-arch..."
+  docker buildx create \
+    --name multi-arch \
+    --platform linux/amd64,linux/arm64 \
+    --driver docker-container \
+    --use \
+    --bootstrap
+else
+  echo -e "Use Docker buildx instance multi-arch..."
+fi
+
+echo -e "Building ${DOCKER_IMAGE_NAME} in version ${DOCKER_IMAGE_TAG}..."
 
 docker buildx build \
   --builder multi-arch \
@@ -20,7 +42,10 @@ docker buildx build \
   --provenance false \
   --build-arg "NGINX_VERSION=${NGINX_VERSION}" \
   --platform linux/amd64,linux/arm64 \
-  --tag taniotoshi/docker-jokes:latest \
+  --tag "taniotoshi/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" \
+  --tag "taniotoshi/${DOCKER_IMAGE_NAME}:latest" \
   --sbom true \
   --push \
   "${REPOSITORY_DIR_PATH}"
+
+echo -e "Finished!"
