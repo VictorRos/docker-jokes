f6b5d607e524119891c29c70497bf0d29b694cff
Victor Ros <vros@cegid.com>
2025-07-05 18:39:18 +0200

    Add MegaLinter and refactor structure
    - Add configuration files for `MegaLinter`
    - Add `MegaLinter` version `8.8.0`
    - Move sources for Docker context in src folder
    - Update local script to build Docker image
    - Update Nginx version to `1.29-alpine3.22`

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,28 +5,36 @@
 # Build the Docker image
 
 # Error handling
-set -Eeuo pipefail
+set -Eeuo pipefail # cspell:disable-line
 
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 REPOSITORY_DIR_PATH="$(cd -- "${SCRIPT_DIR}/.." || return > /dev/null 2>&1 && pwd -P)"
 
 # Variables
-NGINX_VERSION="$(jq -r '.config.nginxVersion' version.json)"
-DOCKER_IMAGE_NAME="$(jq -r '.name' version.json)"
-DOCKER_IMAGE_TAG="$(jq -r '.version' version.json)"
+NGINX_VERSION="$(jq -r '.config.nginxVersion' package.json)"
+DOCKER_IMAGE_NAME="$(jq -r '.name' package.json)"
+DOCKER_IMAGE_TAG="$(jq -r '.version' package.json)"
 
-echo -e "Build Docker image ${DOCKER_IMAGE_NAME}..."
+# Constants
+DOCKER_REGISTRY="taniotoshi"
+DOCKER_PLATFORMS="linux/amd64,linux/arm64"
+
+echo -e "Build Docker image...\n"
+
+echo -e "Nginx version     : ${NGINX_VERSION}"
+echo -e "Docker image name : ${DOCKER_IMAGE_NAME}"
+echo -e "Docker image tag  : ${DOCKER_IMAGE_TAG}\n"
 
 # Check if multi-arch buildx instance exists
-isBuildxInstanceMultiArchExist=$(docker buildx ls | { grep -q "multi-arch" && echo "true" || echo "false"; })
+is_buildx_instance_multi_arch_exist=$(docker buildx ls | { grep -q "multi-arch" && echo "true" || echo "false"; })
 
 # Create multi-arch buildx instance if it does not exist
-if [ "${isBuildxInstanceMultiArchExist}" = "false" ]; then
+if [ "${is_buildx_instance_multi_arch_exist}" = "false" ]; then
   echo -e "Creating Docker buildx instance multi-arch..."
   docker buildx create \
     --name multi-arch \
-    --platform linux/amd64,linux/arm64 \
+    --platform "${DOCKER_PLATFORMS}" \
     --driver docker-container \
     --use \
     --bootstrap
@@ -34,18 +42,18 @@ else
   echo -e "Use Docker buildx instance multi-arch..."
 fi
 
-echo -e "Building ${DOCKER_IMAGE_NAME} in version ${DOCKER_IMAGE_TAG}..."
+echo -e "Building and publishing ${DOCKER_IMAGE_NAME} in version ${DOCKER_IMAGE_TAG}..."
 
 docker buildx build \
   --builder multi-arch \
   --progress auto \
   --provenance false \
   --build-arg "NGINX_VERSION=${NGINX_VERSION}" \
-  --platform linux/amd64,linux/arm64 \
-  --tag "taniotoshi/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" \
-  --tag "taniotoshi/${DOCKER_IMAGE_NAME}:latest" \
+  --platform "${DOCKER_PLATFORMS}" \
+  --tag "${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" \
+  --tag "${DOCKER_REGISTRY}/${DOCKER_IMAGE_NAME}:latest" \
   --sbom true \
   --push \
-  "${REPOSITORY_DIR_PATH}"
+  "${REPOSITORY_DIR_PATH}/src"
 
 echo -e "Finished!"
